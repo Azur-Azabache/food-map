9434496141094a9f7eacdef93fc833a68a395426
Azur_Azabache <tharuka_20@hotmail.com>
2018-01-08 03:34:30 -0500

    subiendo primera vista

---
+++ js/data.js
@@ -0,0 +1,9 @@
+var restaurantes = {
+  'comida peruana': [
+     {imagen: 'b.aromasperuanos.peru1.png',
+      nombre: 'Wayra',
+      mapa: '<iframe class="img-responsive" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3901.191182460743!2d-77.01525468573189!3d-12.099060646168418!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105c877ac11be63%3A0x26764ba130df3487!2sAromas+Peruanos!5e0!3m2!1ses!2spe!4v1514737107243" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>',
+      web: 'https://aromasperuanos.com/web/',
+      direccion: 'Av. Guardia Civil 856, San Isidro 15036',
+      numero: '(01) 2241482',}
+]}
