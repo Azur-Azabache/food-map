41bd492ba7c6aaf3d7d23fc29deac41e3029495f
Azur_Azabache <tharuka_20@hotmail.com>
2018-01-08 04:13:24 -0500

    html + vista principal

--- js/data.js
+++ js/data.js
@@ -1,9 +1,26 @@
 var restaurantes = {
   'comida peruana': [
-     {imagen: 'b.aromasperuanos.peru1.png',
+     {imagen: 'huancaina.jpg',
       nombre: 'Wayra',
-      mapa: '<iframe class="img-responsive" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3901.191182460743!2d-77.01525468573189!3d-12.099060646168418!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105c877ac11be63%3A0x26764ba130df3487!2sAromas+Peruanos!5e0!3m2!1ses!2spe!4v1514737107243" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>',
-      web: 'https://aromasperuanos.com/web/',
-      direccion: 'Av. Guardia Civil 856, San Isidro 15036',
-      numero: '(01) 2241482',}
-]}
+      mapa: '<iframe src="https://www.google.com/maps/embed?pb=!1m22!1m12!1m3!1d124849.49343450459!2d-77.1218534508127!3d-12.0746809428258!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m7!3e6!4m0!4m4!1s0x9105c8e486270727%3A0xf61cdff6835e805d!3m2!1d-12.074689699999999!2d-77.0518129!5e0!3m2!1ses-419!2spe!4v1515401507341" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>',
+      web: 'https://www.facebook.com/wayra.cocinaperuana/',
+      direccion: 'General Garzón 1470, Jesús María 15072',
+      numero: '994 664 705',}
+]
+'comida mexicana': [
+   {imagen: 'tacos.jpg',
+    nombre: 'No te rajes',
+    mapa: '<iframe src="https://www.google.com/maps/embed?pb=!1m22!1m12!1m3!1d124825.70936424665!2d-77.0913487502804!3d-12.125598908124607!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m7!3e6!4m0!4m4!1s0x9105c80337dfcb07%3A0xdd3b73f412ecf315!3m2!1d-12.1256077!2d-77.02130819999999!5e0!3m2!1ses-419!2spe!4v1515401639216" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>',
+    web: 'https://www.facebook.com/lostacosnoterajes',
+    direccion: 'Av. Alfredo Benavides 1083, Miraflores',
+    numero: '959 511 813',}
+]
+'comida japonesa': [
+   {imagen: 'makis.jpg',
+    nombre: 'Edo Sushi',
+    mapa: '<iframe src="https://www.google.com/maps/embed?pb=!1m22!1m12!1m3!1d124828.19696515263!2d-77.10594115033608!3d-12.120283211746017!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m7!3e6!4m0!4m4!1s0x9105c822d93982fb%3A0x379c1b9b24b17350!3m2!1d-12.120292!2d-77.03590059999999!5e0!3m2!1ses-419!2spe!4v1515401880155" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>',
+    web: 'https://edosushibar.com/',
+    direccion: 'Calle Berlin 601, Miraflores 15074',
+    numero: '2432448',}
+]
+}
